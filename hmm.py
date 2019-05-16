e7b0af8b4ebcc21e7623c734a434a470e2681b1f
thuan bao <thuan@axlehire.com>
2019-05-15 19:21:43 -0700

    Add accuracy

--- hmm.py
+++ hmm.py
@@ -1,11 +1,11 @@
+import datetime
+
 import numpy as np
 from hmmlearn import hmm
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 
-from utils import read_file_and_convert_to_np_array, convert_array_to_str
-from sklearn import preprocessing
+from utils import read_file_and_convert_to_np_array
 
 ct_data = np.array(read_file_and_convert_to_np_array('ciphers/column_transposition_cipher'))
 h_data = np.array(read_file_and_convert_to_np_array('ciphers/hill_cipher'))
@@ -14,6 +14,8 @@ ss_data = np.array(read_file_and_convert_to_np_array('ciphers/simple_substitutio
 v_data = np.array(read_file_and_convert_to_np_array('ciphers/vigenere_cipher'))
 
 result = []
+data_point_sets = []
+center_points = []
 
 
 class DataPoint:
@@ -23,60 +25,63 @@ class DataPoint:
         self.score = [0, 0, 0, 0, 0]
 
 
-data_points = []
-
-
-def train_hmm(data, sequence_length, index):
-    model = hmm.MultinomialHMM(n_components=26, init_params="ste", n_iter=2, verbose=True)
-    train_data, test_data = train_test_split(data, test_size=0.05, shuffle=True)
-    # train = np.concatenate(train_data, axis=0)
-    # train = np.concatenate((train_data[0], train_data[1], train_data[2], train_data[3], train_data[4]), axis=0)
-    # length = [len(train_data[0]), len(train_data[1])]
-    # model.fit(np.array([train]).T)
+def train_hmm(data, sequence_length, n_iter, test_size, index):
+    model = hmm.MultinomialHMM(n_components=26, init_params="ste", n_iter=n_iter, verbose=True)
+    train_data, test_data = train_test_split(data, test_size=test_size, shuffle=True)
     if sequence_length != 0:
         train = np.concatenate(train_data, axis=0)[:sequence_length]
     else:
         train = np.concatenate(train_data, axis=0)
-    model.fit(np.array([LabelEncoder().fit_transform(train)]).T)
+    model.fit(np.array([train]).T)
 
+    test_result = []
     for text in test_data:
-        # score = model.decode(np.array([text]).T)
-        # result.append([score, index])
-        # print(model.decode(np.array([test_data[0]]).T, algorithm="viterbi"))
-        # score = model.score(np.array([text]).T)
         data_point = DataPoint(text, index)
-        #     result.append(data_point)
-        data_points.append(data_point)
-    return model
+        test_result.append(data_point)
+    return model, test_result
 
 
-# print('input = ', convert_array_to_str(test_data[0]))
-# result = model.decode(np.array([test_data[0]]).T, algorithm="viterbi")
-# print('output = ', convert_array_to_str(result[1]))
-
-def train_and_score(dataset, sequence_length):
-    # for index, data in enumerate([ct_data, h_data, pf_data, ss_data, v_data]):
+def train_and_score(dataset, sequence_length, n_iter, test_size):
     models = []
-    # for index, data in enumerate([ct_data]):
+    # train 5 models
     for index, data in enumerate(dataset):
-        model = train_hmm(data, sequence_length, index)
+        model, test_result = train_hmm(data, sequence_length, n_iter, test_size, index)
         models.append(model)
-    #
-    for data_point in data_points:
-        scores = [data_point.label]
-        for index, model in enumerate(models):
-            score = model.score(np.array([LabelEncoder().fit_transform(data_point.cipher_text)]).T)
-            scores.append(score)
-        result.append(scores)
-    np.savetxt("abc.txt", result, fmt=["%d", "%s", "%s", "%s", "%s", "%s"], delimiter=', ')
+        data_point_sets.append(test_result)
+
+    # score the sample
+    for data_points in data_point_sets:
+        for ind, data_point in enumerate(data_points):
+            scores = [data_point.label]
+            center_point = []
+            for index, model in enumerate(models):
+                score = model.score(np.array([data_point.cipher_text]).T)
+                scores.append(score)
+                center_point.append(score)
+            result.append(scores)
+            if ind == 0:
+                center_points.append(center_point)
+    file_name = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%m") + '.txt'
+    np.savetxt(file_name, result, fmt=["%d", "%10.2f", "%10.2f", "%10.2f", "%10.2f", "%10.2f"], delimiter=', ')
+    return file_name
 
 
-train_and_score([ct_data, h_data, pf_data, ss_data, v_data], 10000)
-dataset = np.loadtxt("abc.txt", delimiter=', ')
-# X = dataset[:, 0].reshape(5, 100).T
-X = dataset[:, 1:]
-kmeans = KMeans(5, random_state=0)
+test_size = 0.05
+file_name = train_and_score([ct_data, h_data, pf_data, ss_data, v_data], 0, 50, test_size)
+# samples = np.loadtxt("2019-05-15 18:40:05.txt", delimiter=', ')
+samples = np.loadtxt(file_name, delimiter=', ')
+X = samples[:, 1:]
+init_centers = np.array(center_points)
+kmeans = KMeans(5, init=init_centers,  max_iter=1000)
 
-model = kmeans.fit(X)
-labels = model.predict(X)
+kmeans_model = kmeans.fit(X)
+labels = kmeans_model.predict(X)
 print(labels)
+
+number_of_text = int(test_size * 1000)
+for i in range(0, 5):
+    correct_count = 0
+    for j in range(0, number_of_text * (i + 1)):
+        if int(labels[j]) == i:
+            correct_count += 1
+    print("accuracy for {} is: {}".format(i, correct_count / number_of_text))
